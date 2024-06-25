9a60e5aa36393f680a56cdbe95f5e0393c93b405
vansencool <vansencool@gmail.com>
2024-06-25 21:44:26 +0530

    Version checker

--- src/main/java/com/maximde/pluginsimplifier/PluginSimplifier.java
+++ src/main/java/com/maximde/pluginsimplifier/PluginSimplifier.java
@@ -1,9 +1,12 @@
 package com.maximde.pluginsimplifier;
 
+import com.maximde.pluginsimplifier.annotations.MainClassInstance;
 import com.maximde.pluginsimplifier.commands.CommandRegistry;
 import com.maximde.pluginsimplifier.events.EventsRegistry;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.lang.reflect.Field;
 
 public abstract class PluginSimplifier extends JavaPlugin {
 
@@ -13,6 +16,14 @@ public abstract class PluginSimplifier extends JavaPlugin {
     @Override
     public void onEnable() {
         pluginInstance = this;
+        initializeInstance();
+
+        try {
+            getClass().getDeclaredMethod("onStart");
+            onStart();
+        } catch (NoSuchMethodException ignored) {
+
+        }
 
         CommandRegistry.registerCommands();
         EventsRegistry.registerEvents();
@@ -20,8 +31,37 @@ public abstract class PluginSimplifier extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        try {
+            getClass().getDeclaredMethod("onStop");
+            onStop();
+        } catch (NoSuchMethodException ignored) {
+
+        }
+    }
+
+    protected void onStart() {
 
     }
 
+    protected void onStop() {
 
+    }
+
+    private void initializeInstance() {
+        for (Field field : getClass().getDeclaredFields()) {
+            if (field.isAnnotationPresent(MainClassInstance.class)) {
+                if (PluginSimplifier.class.isAssignableFrom(field.getType())) {
+                    field.setAccessible(true);
+                    try {
+                        field.set(this, this);
+                    } catch (IllegalAccessException e) {
+                        getLogger().severe("Could not initialize field annotated with @MainClassInstance: " + field.getName());
+                        e.printStackTrace();
+                    }
+                } else {
+                    getLogger().warning("Field annotated with @MainClassInstance must be of type PluginSimplifier: " + field.getName());
+                }
+            }
+        }
+    }
 }
