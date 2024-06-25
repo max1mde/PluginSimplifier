3489f1b80ed8497c723203828c3fcf65513d91bd
Maxim <maximdeclient@gmail.com>
2024-06-25 14:46:33 +0200

    Add spigot dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven ("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
 }
 
 dependencies {
     compileOnly ("org.projectlombok:lombok:1.18.32")
     annotationProcessor ("org.projectlombok:lombok:1.18.32")
+    compileOnly ("org.spigotmc:spigot-api:1.20.6-R0.1-SNAPSHOT")
 }
 
--- src/main/java/com/maximde/pluginsimplifier/PluginSimplifier.java
+++ src/main/java/com/maximde/pluginsimplifier/PluginSimplifier.java
@@ -1,4 +1,5 @@
 package com.maximde.pluginsimplifier;
 
 public class PluginSimplifier {
+
 }
