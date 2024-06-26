f844b73e59e1cde72a362c322c6b3905419685c1
vansencool <vansencool@gmail.com>
2024-06-26 22:34:22 +0530

    Fixed a small issue with the PluginSimplifier class

--- src/main/java/com/maximde/pluginsimplifier/PluginSimplifier.java
+++ src/main/java/com/maximde/pluginsimplifier/PluginSimplifier.java
@@ -1,17 +1,11 @@
 package com.maximde.pluginsimplifier;
 
 import com.maximde.pluginsimplifier.annotations.MainClassInstance;
-import com.maximde.pluginsimplifier.commands.CommandRegistry;
-import com.maximde.pluginsimplifier.events.EventsRegistry;
-import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.lang.reflect.Field;
 
 public abstract class PluginSimplifier extends JavaPlugin {
-
-    @Getter
-    private static PluginSimplifier pluginInstance;
 
     @Override
     public void onEnable() {
@@ -23,9 +17,6 @@ public abstract class PluginSimplifier extends JavaPlugin {
         } catch (NoSuchMethodException ignored) {
 
         }
-
-        CommandRegistry.registerCommands();
-        EventsRegistry.registerEvents();
     }
 
     @Override
