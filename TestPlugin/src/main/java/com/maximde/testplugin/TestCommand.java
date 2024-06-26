16ccaae20a8a6cb08c4ee3bee8338ea42c2150ec
Maxim <maximdeclient@gmail.com>
2024-06-26 18:10:32 +0200

    Impl test command

--- TestPlugin/src/main/java/com/maximde/testplugin/TestCommand.java
+++ TestPlugin/src/main/java/com/maximde/testplugin/TestCommand.java
@@ -1,4 +1,16 @@
 package com.maximde.testplugin;
 
-public class TestCommand {
+import com.maximde.pluginsimplifier.annotations.Register;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class TestCommand implements CommandExecutor {
+
+    @Register("test")
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        commandSender.sendMessage("Works!");
+        return false;
+    }
 }
--- TestPlugin/src/main/java/com/maximde/testplugin/TestPlugin.java
+++ TestPlugin/src/main/java/com/maximde/testplugin/TestPlugin.java
@@ -5,14 +5,5 @@ import com.maximde.pluginsimplifier.PluginSimplifier;
 
 public final class TestPlugin extends PluginSimplifier {
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
 
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
