4c22f59b7272599701221d58b69729b115b9fc5e
vansencool <vansencool@gmail.com>
2024-08-02 19:55:02 +0530

    Exclude kotlin.* and org.jetbrains.* | org.intellij.*

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.maximde"
-version = "1.0.5"
+version = "1.0.6"
 
 repositories {
     mavenCentral()
@@ -13,11 +13,18 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.20")
+    compileOnly("org.jetbrains:annotations:24.0.1")
 }
 
 tasks.jar {
     archiveBaseName.set("PluginSimplifier")
     archiveVersion.set(version.toString())
+
+    // Exclude kotlin and org.jetbrains packages
+    exclude("kotlin/**")
+    exclude("org/intellij/**")
+    exclude("org/jetbrains/**")
 }
 
 publishing {
