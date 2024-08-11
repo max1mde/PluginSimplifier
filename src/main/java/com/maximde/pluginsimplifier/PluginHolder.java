8f9c8b8520f225a4b7dd7ebdcd6278ca18975300
vansencool <vansencool@gmail.com>
2024-08-11 22:20:31 +0530

    Throws a RuntimeException if plugin instance is not set

--- src/main/java/com/maximde/pluginsimplifier/PluginHolder.java
+++ src/main/java/com/maximde/pluginsimplifier/PluginHolder.java
@@ -3,9 +3,12 @@ package com.maximde.pluginsimplifier;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PluginHolder {
-    private static JavaPlugin pluginInstance;
+    private static JavaPlugin pluginInstance = null;
 
     public static JavaPlugin getPluginInstance() {
+        if (pluginInstance == null) {
+            throw new RuntimeException("Something tried to get the plugin instance while its not set");
+        }
         return pluginInstance;
     }
 
