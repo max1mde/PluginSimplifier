d73bd55e96bc9df65dcbc928ab7fc791b22fcd30
vansencool <vansencool@gmail.com>
2024-08-13 21:02:48 +0530

    Added javadocs

--- src/main/java/com/maximde/pluginsimplifier/command/CommandRegistrar.java
+++ src/main/java/com/maximde/pluginsimplifier/command/CommandRegistrar.java
@@ -25,10 +25,27 @@ public class CommandRegistrar {
         }
     }
 
+    /**
+     * Creates a new {@link CommandBuilder} instance for defining a command.
+     *
+     * <p>This method checks that neither the command name nor the executor is null,
+     * throwing an exception if either is found to be so. After checking, it returns a
+     * new {@code CommandBuilder} instance configured with the provided name and executor.
+     *
+     * @param name     The name of the command to be registered.
+     * @param executor The {@link CommandExecutor} responsible for executing the command logic.
+     * @return A {@link CommandBuilder} instance for further configuration of the command.
+     * @throws NullPointerException if either the command name or executor is null.
+     */
     public static CommandBuilder command(String name, CommandExecutor executor) {
+        if (name == null) throw new NullPointerException("Command name cannot be null");
+        if (executor == null) throw new NullPointerException("Command executor cannot be null");
         return new CommandBuilder(name, executor);
     }
 
+    /**
+     * The CommandBuilder class allows for the configuration and registration of a command.
+     */
     public static class CommandBuilder {
         private final String name;
         private final CommandExecutor executor;
@@ -44,36 +61,75 @@ public class CommandRegistrar {
             this.executor = executor;
         }
 
+        /**
+         * Sets the namespace for the command. Default uses plugin's name as the namespace
+         *
+         * @param namespace The namespace to set.
+         * @return This CommandBuilder instance for method chaining.
+         */
         public CommandBuilder namespace(String namespace) {
             this.namespace = namespace;
             return this;
         }
 
+        /**
+         * Sets the usage message for the command.
+         *
+         * @param usage The usage message to set.
+         * @return This CommandBuilder instance for method chaining.
+         */
         public CommandBuilder usage(String usage) {
             this.usage = usage;
             return this;
         }
 
+        /**
+         * Sets the description for the command.
+         *
+         * @param description The description to set.
+         * @return This CommandBuilder instance for method chaining.
+         */
         public CommandBuilder description(String description) {
             this.description = description;
             return this;
         }
 
+        /**
+         * Sets the aliases for the command.
+         *
+         * @param aliases The list of aliases to set.
+         * @return This CommandBuilder instance for method chaining.
+         */
         public CommandBuilder aliases(List<String> aliases) {
             this.aliases = aliases;
             return this;
         }
 
+        /**
+         * Sets the permission required to execute the command.
+         *
+         * @param permission The permission to set.
+         * @return This CommandBuilder instance for method chaining.
+         */
         public CommandBuilder permission(String permission) {
             this.permission = permission;
             return this;
         }
 
+        /**
+         * Sets the TabCompleter for the command, which handles tab completion logic.
+         *
+         * @param completer The TabCompleter to set.
+         * @return This CommandBuilder instance for method chaining.
+         */
         public CommandBuilder completer(TabCompleter completer) {
             this.completer = completer;
             return this;
         }
 
+        /**
+         * Registers the command itself. This method must be called to make the command available.
+         */
         public void register() {
             JavaPlugin plugin = PluginHolder.getPluginInstance();
             CustomCommand command = new CustomCommand(name, executor, completer);
