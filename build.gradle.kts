3a138b216a933926ffe78ed1e3ad05ea97902587
vansencool <vansencool@gmail.com>
2024-08-13 15:28:11 +0530

    paper -> spigot

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,12 @@ group = "com.maximde"
 version = "1.0.7"
 
 repositories {
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.20.4-R0.1-SNAPSHOT")
     compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.20")
     compileOnly("org.jetbrains:annotations:24.0.1")
 }
