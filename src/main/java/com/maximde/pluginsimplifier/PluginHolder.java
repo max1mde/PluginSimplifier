70e35ab1574d8fee5011230e74c25c67805bb20d
vansencool <vansencool@gmail.com>
2024-08-14 11:50:37 +0530

    Added javadocs and null checks

--- src/main/java/com/maximde/pluginsimplifier/PluginHolder.java
+++ src/main/java/com/maximde/pluginsimplifier/PluginHolder.java
@@ -2,17 +2,37 @@ package com.maximde.pluginsimplifier;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Holds an instance of {@link JavaPlugin}
+ */
 public class PluginHolder {
     private static JavaPlugin pluginInstance = null;
 
+    /**
+     * Retrieves the currently held instance of {@link JavaPlugin}.
+     *
+     * @return The current instance of {@link JavaPlugin}, never null after being set.
+     * @throws NullPointerException If this method is called before setting an instance via {@link #setPluginInstance(JavaPlugin)}.
+     * @see #setPluginInstance(JavaPlugin)
+     */
     public static JavaPlugin getPluginInstance() {
         if (pluginInstance == null) {
-            throw new RuntimeException("Something tried to get the plugin instance while its not set");
+            throw new NullPointerException("Something tried to get the plugin instance while its not set");
         }
         return pluginInstance;
     }
 
+    /**
+     * Sets the instance of {@link JavaPlugin} to be held by this class.
+     *
+     * @param pluginInstance The instance of {@link JavaPlugin} to hold.
+     * @throws NullPointerException If the pluginInstance is null
+     * @see #getPluginInstance()
+     */
     public static void setPluginInstance(JavaPlugin pluginInstance) {
+        if (pluginInstance == null) {
+            throw new NullPointerException("Something tried to set a null plugin instance");
+        }
         PluginHolder.pluginInstance = pluginInstance;
     }
 }
